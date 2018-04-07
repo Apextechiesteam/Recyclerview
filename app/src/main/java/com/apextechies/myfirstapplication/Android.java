1bf8c54ed438aafc4bc0601bb33b0b2cbd295f57
Shankar1056 <dayashankargupta86@gmail.com>
2018-04-07 15:05:40 +0530

    spinner

--- app/src/main/java/com/apextechies/myfirstapplication/Android.java
+++
@@ -1,8 +0,0 @@
-package com.apextechies.myfirstapplication;
-
-/**
- * Created by Shankar on 3/25/2018.
- */
-
-public class Android {
-}
--- app/src/main/java/com/apextechies/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/apextechies/myfirstapplication/MainActivity.java
@@ -2,17 +2,39 @@ package com.apextechies.myfirstapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
-    Button buttom;
+    String[] country = { "Select Country", "India", "USA", "China", "Japan", "Other",  };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spin = (Spinner) findViewById(R.id.spinner1);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(),country[position] ,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
 
     }
 }
